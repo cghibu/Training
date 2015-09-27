4b1e474800e3a518686e0c0d2bdf367834d6869d
Calin Ghibu <cghibu@gmail.com>
2015-09-27 22:44:43 +0300

    all done & clean

---
+++ UnitTestProject3/DobandaDescrescatoare.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject3
+{
+    [TestClass]
+    public class DobandaDescrescatoare
+    {
+        public double CalculateLoanDescending(double amount, double yearlyrate, double periodinmonths, uint monthtocalculate)
+        {
+
+            if (amount > 0 && yearlyrate >= 0 && periodinmonths > 0 && monthtocalculate > 0 && monthtocalculate <= periodinmonths)
+            {
+                double amountpermonth = Math.Round(amount / periodinmonths, 3);
+                double interestratepermonth = Math.Round(yearlyrate / 12, 3);
+                double interestpermonth = Math.Round(((interestratepermonth / 100) * (periodinmonths - monthtocalculate + 1) * amountpermonth), 3);
+                return amountpermonth + interestpermonth;
+            }
+            return 0;
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            double amount = 100000;
+            double yearlyrate = 7.5;
+            double periodinmonths = 60;
+            uint monthtocalculate = 5;
+
+            double result = CalculateLoanDescending(amount, yearlyrate, periodinmonths, monthtocalculate);
+            Assert.AreEqual(2250, result, 0.01);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            double amount = -1;
+            double yearlyrate = 7.5;
+            double periodinmonths = 60;
+            uint monthtocalculate = 5;
+
+            double result = CalculateLoanDescending(amount, yearlyrate, periodinmonths, monthtocalculate);
+            Assert.AreEqual(0, result, 0.01);
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+
+            double amount = 1;
+            double yearlyrate = -7.5;
+            double periodinmonths = 60;
+            uint monthtocalculate = 5;
+
+            double result = CalculateLoanDescending(amount, yearlyrate, periodinmonths, monthtocalculate);
+            Assert.AreEqual(0, result, 0.01);
+
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+
+            double amount = 1;
+            double yearlyrate = 7.5;
+            double periodinmonths = -60;
+            uint monthtocalculate = 5;
+
+            double result = CalculateLoanDescending(amount, yearlyrate, periodinmonths, monthtocalculate);
+            Assert.AreEqual(0, result, 0.01);
+
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+
+            double amount = 100000;
+            double yearlyrate = 7.5;
+            double periodinmonths = 60;
+            uint monthtocalculate = 1;
+
+            double result = CalculateLoanDescending(amount, yearlyrate, periodinmonths, monthtocalculate);
+            Assert.AreEqual(2291.667, result, 0.01);
+
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+
+            double amount = 100000;
+            double yearlyrate = 7.5;
+            double periodinmonths = 60;
+            uint monthtocalculate = 60;
+
+            double result = CalculateLoanDescending(amount, yearlyrate, periodinmonths, monthtocalculate);
+            Assert.AreEqual(1677.083, result, 0.01);
+
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+
+            double amount = 100000;
+            double yearlyrate = 100;
+            double periodinmonths = 60;
+            uint monthtocalculate = 35;
+
+            double result = CalculateLoanDescending(amount, yearlyrate, periodinmonths, monthtocalculate);
+            Assert.AreEqual(5277.778, result, 0.01);
+
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+
+            double amount = 100000;
+            double yearlyrate = 7.5;
+            double periodinmonths = 60;
+            uint monthtocalculate = 120;
+
+            double result = CalculateLoanDescending(amount, yearlyrate, periodinmonths, monthtocalculate);
+            Assert.AreEqual(0, result, 0.01);
+
+        }
+    }
+    }
---
+++ UnitTestProject3/parchet.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject3
+{
+    [TestClass]
+    public class parchet
+    {
+        public uint CalculatePlaques(double room_length, double room_width, double plaque_length, double plaque_width) {
+
+            if (room_length <= 0 || room_width <= 0 || plaque_length <= 0 || plaque_width <= 0) return 0;
+            double room_area = room_length * room_width;
+            double plaque_area = plaque_length * plaque_width;
+            if (room_area >= plaque_area)
+                    {
+                        double result = Math.Ceiling((room_area / plaque_area) + (room_area / plaque_area) * 15 / 100);
+                        return (uint)result;
+                    }
+            else return 1;
+                     
+     }
+
+
+        [TestMethod]
+        public void TestParchet1() //all 0
+        {
+            double room_length = 0;
+            double room_width = 0;
+            double plaque_length = 0;
+            double plaque_width = 0;
+
+            uint result = CalculatePlaques(room_length, room_width, plaque_length, plaque_width);
+            Assert.AreEqual((uint)0, result);
+
+
+
+        }
+
+        [TestMethod]
+        public void TestParchet2() // some natural values
+        {
+            uint room_length = 10;
+            uint room_width = 15;
+            uint plaque_length = 2;
+            uint plaque_width = 1;
+
+            uint result = CalculatePlaques(room_length, room_width, plaque_length, plaque_width);
+            Assert.AreEqual((uint)87, result);
+
+
+
+        }
+        [TestMethod] 
+        public void TestParchet3() // some real values
+        {
+            double room_length = 10.3;
+            double room_width = 15.1;
+            double plaque_length = 2.25;
+            double plaque_width = 1.6;
+
+            uint result = CalculatePlaques(room_length, room_width, plaque_length, plaque_width);
+            Assert.AreEqual((uint)50, result);
+
+        }
+
+        [TestMethod]
+        public void TestParchet4() //all negatives
+        {
+            double room_length = -10.3;
+            double room_width = -15.1;
+            double plaque_length = -2.25;
+            double plaque_width = -1.6;
+
+            uint result = CalculatePlaques(room_length, room_width, plaque_length, plaque_width);
+            Assert.AreEqual((uint)0, result);
+
+        }
+        [TestMethod]
+        public void TestParchet5() //one dimension negative for both
+        {
+            double room_length = 10.3;
+            double room_width = -15.1;
+            double plaque_length = 2.25;
+            double plaque_width = -1.6;
+
+            uint result = CalculatePlaques(room_length, room_width, plaque_length, plaque_width);
+            Assert.AreEqual((uint)0, result);
+
+        }
+        [TestMethod]
+        public void TestParchet6() // room smaller than plaque
+        {
+            double room_length = 2;
+            double room_width = 3.5;
+            double plaque_length = 8;
+            double plaque_width = 1.2;
+
+            uint result = CalculatePlaques(room_length, room_width, plaque_length, plaque_width);
+            Assert.AreEqual((uint)1, result);
+
+        }
+
+    }
+}
