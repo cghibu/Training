c53963d95319895e49dd269a4a9118e1eb582e4f
Calin Ghibu <cghibu@gmail.com>
2015-09-27 10:27:33 +0300

    student gata

---
+++ UnitTestProject3/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject3
+{
+    [TestClass]
+    public class Student
+    {
+        public uint CateRepetari(uint n) {
+
+            uint result = n * n;
+            return result;
+
+        }
+
+
+        [TestMethod]
+        public void TestStudent1()
+        {
+
+            uint n = 21;
+           
+            uint result = CateRepetari(n);
+
+            Assert.AreEqual((uint)441, result);
+
+
+        }
+        [TestMethod]
+        public void TestStudent2()
+        {
+
+            uint n = 1;
+
+            uint result = CateRepetari(n);
+
+            Assert.AreEqual((uint)1, result);
+
+
+        }
+        [TestMethod]
+        public void TestStudent3()
+        {
+
+            uint n = 0;
+
+            uint result = CateRepetari(n);
+
+            Assert.AreEqual((uint)0, result);
+
+
+        }
+    }
+}
